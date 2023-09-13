5f31e4678471157e3812c16d6861b11465831815
Cicero Cauã <126025158+Caua017@users.noreply.github.com>
2023-09-12 21:56:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const key = "17a700902ace013a280d9ec0a412d52a"
+
+
+function colocarDadosNaTela(dados){
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+}   
+
+async function buscarCidade(cidade){
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())   
+    
+    colocarDadosNaTela(dados)
+}
+
+
+function cliqueiNoBotao(){
+    const cidade = document.querySelector(".input-city").value
+ 
+    buscarCidade(cidade)
+
+}
+
+
+
+
